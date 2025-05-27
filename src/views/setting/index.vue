f36c75dfe09716e8f5fad8968b8850bd410ae741
unknown <qianjiecheng@hikvision.com>
2025-05-27 20:09:00 +0800

    feat: 设置界面支持选择语言 todo: 支持国际化

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import useConfig from '@/stores/modules/config'
 import useUserStore from '@/stores/modules/user'
+import type { Numeric } from 'vant/lib/utils'
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 
@@ -14,6 +15,13 @@ const router = useRouter()
 
 const isDark = ref(config.isDark)
 
+const languagePickerShow = ref(false)
+const languagePickerValue = ref<Numeric[]>([]);
+const languageColumns = [
+  { text: '中文', value: 'zh-CN' },
+  { text: '英文', value: 'en-US' },
+]
+
 const toggleTheme = (val: boolean) => {
   isDark.value = val
   config.toggleDark()
@@ -22,6 +30,20 @@ const toggleTheme = (val: boolean) => {
 const logout = () => {
   userStore.logout()
   router.push('/login')
+}
+
+const onConfirm = (val: { selectedValues: Numeric[] }) => {
+  console.log("@@@", val);
+  languagePickerValue.value = val.selectedValues
+  closeLanguagePop()
+}
+
+const showLanguagePop = () => {
+  languagePickerShow.value = true
+}
+
+const closeLanguagePop = () => {
+  languagePickerShow.value = false
 }
 </script>
 
@@ -42,10 +64,16 @@ const logout = () => {
         <van-switch v-model="isDark" @change="toggleTheme" />
       </template>
     </van-cell>
+    <van-cell title="语言" is-link icon="shop-o" :value="languagePickerValue[0]" @click="showLanguagePop"></van-cell>
   </van-cell-group>
   <van-cell-group inset title=" ">
     <van-cell title="退出系统" @click="logout" />
   </van-cell-group>
+
+  <van-popup v-model:show="languagePickerShow" position="bottom">
+    <van-picker :model-value="languagePickerValue" title="语言" :columns="languageColumns" @confirm="onConfirm"
+      @cancel="closeLanguagePop" />
+  </van-popup>
 </template>
 
 <style lang="less">
